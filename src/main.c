83b3b68d2c671b9699efd65f67a35ac703f9fb1e
Adarsh Jha <adarsh2005j@gmail.com>
2026-05-03 01:44:19 +0530

    Really interesting things.....POSIX hmmmmmmmmmmmmmm

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
@@ -10,7 +18,35 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    const char *input_file_path = argv[0];
-    (void)input_file_path;
+    const char *input_file_path = argv[1];
+
+    int fd = open(input_file_path, O_RDONLY);
+    if (fd < 0)
+    {
+        fprintf(stderr, "ERROR: could not open file %s: %s\n", input_file_path, strerror(errno));
+        exit(1);
+    }
+
+    struct stat statbuf = {0};
+    if (fstat(fd, &statbuf) < 0)
+    {
+        fprintf(stderr, "ERROR: could not get thr size of the file %s: %s\n", input_file_path, strerror(errno));
+        exit(1);
+    }
+
+    size_t content_size = statbuf.st_size;
+
+    char *content_data = mmap(NULL, content_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (content_data == NULL)
+    {
+        fprintf(stderr, "ERROR: could not memory map file %s: %s\n", input_file_path, strerror(errno));
+        exit(1);
+    }
+
+    fwrite(content_data, content_size, 1, stdout);
+
+    munmap(content_data, content_size);
+    close(fd);
+
     return 0;
 }
